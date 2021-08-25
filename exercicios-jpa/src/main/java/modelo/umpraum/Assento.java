275ce2a6ead939d45448aed1175dab2135d0f220
Renato Pereira Santos <renatodev95@gmail.com>
2021-08-25 10:17:06 -0300

    estabelecendo relação UM PARA UM

---
+++ exercicios-jpa/src/main/java/modelo/umpraum/Assento.java
@@ -0,0 +1,35 @@
+package modelo.umpraum;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "assentos")
+public class Assento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	public Assento() {
+		
+	}
+	
+	public Assento(String nome) {
+		super();
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
