147e32a9a091959bd8db61a3db0328e02a3f200a
Renato Pereira Santos <renatodev95@gmail.com>
2021-08-29 11:56:47 -0300

    consultas na relação @ManyToMany e passagem de parâmetros
    consultas na relação @ManyToMany e passagem de parâmetros no novo método criando dentro do DAO

--- exercicios-jpa/src/main/java/infra/DAO.java
+++ exercicios-jpa/src/main/java/infra/DAO.java
@@ -1,5 +1,6 @@
 package infra;
 
+import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -15,8 +16,7 @@ public class DAO<E> {
 
 	static {
 		try {
-			emf = Persistence
-					.createEntityManagerFactory("exercicios-jpa");
+			emf = Persistence.createEntityManagerFactory("exercicios-jpa");
 		} catch (Exception e) {
 			// logar -> log4j
 		}
@@ -30,47 +30,57 @@ public class DAO<E> {
 		this.classe = classe;
 		em = emf.createEntityManager();
 	}
-	
+
 	public DAO<E> abrirT() {
 		em.getTransaction().begin();
 		return this;
 	}
-	
+
 	public DAO<E> fecharT() {
 		em.getTransaction().commit();
 		return this;
 	}
-	
+
 	public DAO<E> incluir(E entidade) {
 		em.persist(entidade);
 		return this;
 	}
-	
-	public DAO<E> incluirAtomico(E entidade) {	
+
+	public DAO<E> incluirAtomico(E entidade) {
 		return this.abrirT().incluir(entidade).fecharT();
 	}
-	
+
 	public E obterPorId(Object id) {
 		return em.find(classe, id);
 	}
-	
+
 	public List<E> obterTodos() {
 		return this.obterTodos(10, 0);
 	}
-	
+
 	public List<E> obterTodos(int limite, int deslocamento) {
-		
+
 		if (classe == null) {
 			throw new UnsupportedOperationException("Classe nula.");
 		}
-		
+
 		String jpql = "select e from " + classe.getName() + " e";
 		TypedQuery<E> query = em.createQuery(jpql, classe);
 		query.setMaxResults(limite);
 		query.setFirstResult(deslocamento);
 		return query.getResultList();
 	}
-	
+
+	public List<E> consultar(String nomeConsulta, Object... params) {
+		TypedQuery<E> query = em.createNamedQuery(nomeConsulta, classe);
+		
+		for (int i = 0; i < params.length; i += 2) {
+			query.setParameter(params[i].toString(), params[i+1]);
+		}
+		
+		return query.getResultList();
+	}
+
 	public void fecharC() {
 		em.close();
 	}
